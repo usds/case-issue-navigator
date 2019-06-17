c1f13eb6ee9ae173c5c28714daac9f0251f33b3f
Ben Warfield <ben.warfield@usds.dhs.gov>
2019-06-16 22:49:11 -0400

    Attribute name translation fix.

--- src/view/PrimaryNavMenu.js
+++ src/view/PrimaryNavMenu.js
@@ -31,7 +31,7 @@ export default function PrimaryNavMenu(props) {
                 <div className="usa-nav__secondary">
                     <form className="usa-search usa-search--small ">
                         <div role="search">
-                            <label className="usa-sr-only" for="extended-search-field-small">Search small</label>
+                            <label className="usa-sr-only" htmlFor="extended-search-field-small">Search small</label>
                             <input className="usa-input" id="extended-search-field-small" type="search" name="search" />
                             <button className="usa-button" type="submit"><span className="usa-sr-only">Search</span></button>
                         </div>
