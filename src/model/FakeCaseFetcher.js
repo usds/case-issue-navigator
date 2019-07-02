73f519c14579f3c9fe89d88e33dd3d82852be685
Ben Warfield <ben.warfield@usds.dhs.gov>
2019-07-02 13:54:08 -0400

    OK, it wasn't *quite* the last reference.

--- src/model/FakeCaseFetcher.js
+++ src/model/FakeCaseFetcher.js
@@ -1,11 +1,6 @@
 import fakedata from "../sample_data/i90_old";
-import fake_resolved from "../sample_data/i90_resolved";
 
 const ageSort = (a,b)=>(b.caseAge - a.caseAge);
-
-export function fetchResolved() {
-    return fake_resolved.sort(ageSort);
-}
 
 export function fetchAll() {
     return fakedata.sort(ageSort);
