d86dd50c492dcaf5b72de96681f7397cfc68783f
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2019-08-09 10:59:57 -0500

    Add cellDispatch tests

--- src/view/util/cellDispatch.js
+++ src/view/util/cellDispatch.js
@@ -3,21 +3,31 @@ import { ELIS_CASE_BASE_URL } from "../../controller/config";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const cellDispatch = {
-  LINK: (r, rowData) => (
-    <>
-      <a href={ELIS_CASE_BASE_URL + r} target="_elis_viewer">
-        {r}
-      </a>
-      {rowData.desnoozed ? (
-        <FontAwesomeIcon
-          icon="exclamation-triangle"
-          className="text-accent-warm"
-        />
-      ) : null}
-    </>
-  ),
+  LINK: (r, rowData) => {
+    if (!r) {
+      return null;
+    }
+
+    return (
+      <>
+        <a href={ELIS_CASE_BASE_URL + r} target="_elis_viewer">
+          {r}
+        </a>
+        {rowData && rowData.desnoozed ? (
+          <FontAwesomeIcon
+            icon="exclamation-triangle"
+            className="text-accent-warm"
+          />
+        ) : null}
+      </>
+    );
+  },
   DATE: d => {
     const datum = new Date(d);
+    if (typeof d !== "string" || isNaN(datum.getMonth())) {
+      return "Invalid date";
+    }
+
     return (
       datum.getMonth() + 1 + "/" + datum.getDate() + "/" + datum.getFullYear()
     );
