6aec65cca33f26ceaac77f30da647749c3db2d26
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2019-09-04 14:05:46 -0400

    Replace remaining instances of caseFetcher with the new API client

--- src/view/caselists/ActiveCaseList.jsx
+++ src/view/caselists/ActiveCaseList.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { CaseList } from "./CaseList";
 import { VIEWS, I90_HEADERS } from "../../controller/config";
-import { approximateDays } from "../util/approximateDays";
-import caseFetcher from "../../model/caseFetcher";
 import { getHeaders } from "../util/getHeaders";
 import SnoozeForm from "../../controller/SnoozeForm";
 import { formatNotes } from "../util/formatNotes";
@@ -31,41 +29,42 @@ const ActiveCaseList = props => {
         message: "There was an error loading cases.",
         type: "error"
       });
-      const errorJson = await response.responseError.getJson();
-      console.error(errorJson);
+      if (response.responseReceived) {
+        const errorJson = await response.responseError.getJson();
+        console.error(errorJson);
+      }
     })(currentPage);
   }, [currentPage, setIsLoading, setNotification]);
 
   const snooze = async (rowData, snoozeOption) => {
     const notes = formatNotes(snoozeOption);
-    try {
-      const snoozeData = await caseFetcher.updateActiveSnooze(
-        rowData.receiptNumber,
-        {
-          duration: snoozeOption.duration,
-          reason: snoozeOption.snoozeReason,
-          notes
-        }
-      );
-
-      const snoozeDays = approximateDays({
-        startDate: snoozeData.snoozeStart,
-        endDate: snoozeData.snoozeEnd
-      });
-
+    const response = await RestAPIClient.cases.updateActiveSnooze(
+      rowData.receiptNumber,
+      {
+        duration: snoozeOption.duration,
+        reason: snoozeOption.snoozeReason,
+        notes
+      }
+    );
+    if (response.succeeded) {
       props.updateSummaryData();
       setNotification({
-        message: `${
-          rowData.receiptNumber
-        } has been Snoozed for ${snoozeDays} day${snoozeDays !== 1 &&
-          "s"} due to ${snoozeData.snoozeReason}.`,
+        message: `${rowData.receiptNumber} has been Snoozed for ${
+          snoozeOption.duration
+        } day${snoozeOption.duration !== 1 ? "s" : ""} due to ${
+          snoozeOption.snoozeReason
+        }.`,
         type: "success"
       });
+      return setCases(
+        cases.filter(c => c.receiptNumber !== rowData.receiptNumber)
+      );
+    }
 
-      setCases(cases.filter(c => c.receiptNumber !== rowData.receiptNumber));
-    } catch (e) {
-      console.error(e.message);
-      setNotification({ message: e.message, type: "error" });
+    setNotification("There was an error updating the case.", "error");
+    if (response.responseReceived) {
+      const errorJson = await response.responseError.getJson();
+      console.error(errorJson);
     }
   };
 
