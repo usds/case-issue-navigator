634c0117deced06bbf2cd870d90f750463ef058e
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2019-09-09 10:31:19 -0400

    Add CASE_MANAGEMENT_SYSTEM and CASE_TYPE environment variables, delete redundant constant declaration in config.js

--- src/model/ClientBase.ts
+++ src/model/ClientBase.ts
@@ -6,8 +6,11 @@ type CaseType = "STANDARD";
 
 class ClientBase extends FetchClient {
   protected resultsPerPage: number = 20;
-  protected caseManagementSystem: CaseManagementSystem = "DEFAULT";
-  protected caseType: CaseType = "STANDARD";
+  protected caseManagementSystem: CaseManagementSystem =
+    (process.env.REACT_APP_CASE_MANAGEMENT_SYSTEM as CaseManagementSystem) ||
+    "DEFAULT";
+  protected caseType: CaseType =
+    (process.env.REACT_APP_CASE_TYPE as CaseType) || "STANDARD";
 
   protected static createApiUrl(endpoint: string) {
     return new URL(endpoint, API_BASE_URL);
