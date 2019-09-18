388eff2ae1b3add54a5352f5de4e65a248ed52b2
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2019-09-18 12:18:42 -0400

    Add case details loading indicator

--- src/view/tables/CaseDetails.tsx
+++ src/view/tables/CaseDetails.tsx
@@ -10,10 +10,12 @@ interface CaseDetailsProps {
 }
 
 const CaseDetails: React.FunctionComponent<CaseDetailsProps> = props => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [caseDetails, setCaseDetails] = useState<Array<CaseDetail>>([]);
   const { receiptNumber } = props;
 
   useEffect(() => {
+    setIsLoading(true);
     const getCaseDetails = async () => {
       const response = await RestAPIClient.cases.getCaseDetails(receiptNumber);
       if (response.succeeded) {
@@ -47,10 +49,22 @@ const CaseDetails: React.FunctionComponent<CaseDetailsProps> = props => {
         setCaseDetails(
           caseEvents.sort((a, b) => b.date.valueOf() - a.date.valueOf())
         );
+        setIsLoading(false);
       }
     };
     getCaseDetails();
   }, [receiptNumber]);
+
+  const detailsSection = () => {
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+    return caseDetails.length === 0 ? (
+      <p>No case details.</p>
+    ) : (
+      <CaseDetailList caseDetails={caseDetails} />
+    );
+  };
 
   return (
     <React.Fragment>
@@ -58,11 +72,7 @@ const CaseDetails: React.FunctionComponent<CaseDetailsProps> = props => {
         <td colSpan={1}></td>
         <td colSpan={props.numberOfColumns - 1}>
           <h3>Case Details</h3>
-          {caseDetails.length === 0 ? (
-            <p>No case details.</p>
-          ) : (
-            <CaseDetailList caseDetails={caseDetails} />
-          )}
+          {detailsSection()}
         </td>
       </tr>
     </React.Fragment>
