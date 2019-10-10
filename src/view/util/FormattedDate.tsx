f9d489faff2833b6acbf53e4394e441ad9e72f87
Tim Best <timothy.best@hq.dhs.gov>
2019-10-10 13:44:31 -0400

    wire up last updated

--- src/view/util/FormattedDate.tsx
+++ src/view/util/FormattedDate.tsx
@@ -2,16 +2,22 @@ import React from "react";
 
 interface Props {
     label: string;
-    date?: Date;
+    date?: string | null;
 }
 
 const FormattedDate: React.FC<Props> = props => {
-
     if (!props.date) {
         return null;
     }
+
+    const date = new Date(props.date);
+
+    if (date.toString() === "Invalid Date") {
+        console.error(`Invalid date: {date}`);
+    }
+
     return (
-        <p>{props.label}: {props.date.toLocaleString()}</p>
+        <p>{props.label}: {date.toLocaleString()}</p>
     );
 };
 
