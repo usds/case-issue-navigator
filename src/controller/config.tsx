8e68276728e238cd9a3241b761630a4c9f7bc8fb
Tim Best <timothy.best@hq.dhs.gov>
2019-10-18 11:50:03 -0400

    update assignee as a single latest

--- src/controller/config.tsx
+++ src/controller/config.tsx
@@ -169,13 +169,15 @@ const I90_HEADERS = [
       if (!notes || !Array.isArray(notes)) {
         return null;
       }
-      return notes
+      const assignees = notes
         .filter(note => note.subType === "assignee")
         .sort((a, b) => {
           return new Date(b.timestamp).valueOf() - new Date(a.timestamp).valueOf();
-        })
-        .map(assignee => assignee.content)
-        .join(", ");
+        });
+      if (assignees.length === 0) {
+        return null;
+      }
+      return assignees[0].content;
     },
     views: [VIEWS.CASES_TO_WORK.TITLE, VIEWS.SNOOZED_CASES.TITLE]
   },
