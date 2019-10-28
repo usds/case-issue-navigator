669014c88e90cb1fe01c887d9aaff481b6da36ef
Tim Best <timothy.best@hq.dhs.gov>
2019-10-28 12:00:45 -0400

    convert table render componets to typescript

--- src/view/tables/ReceiptDisplayRow.tsx
+++ src/view/tables/ReceiptDisplayRow.tsx
@@ -1,23 +1,30 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { TableCell } from "./TableCell";
 import "./ReceiptDisplayRow.scss";
 import { CaseDetails } from "./CaseDetails";
 
-const ReceiptDisplayRow = props => {
+interface Props {
+  headers: Header[];
+  data: Case;
+  callback: any;
+}
+
+const ReceiptDisplayRow = (props: Props) => {
   const { data, callback, headers } = props;
   const numberOfColumns = headers.length;
   return (
     <React.Fragment>
       <tr className={data.showDetails ? "row--show-details" : undefined}>
-        {headers.map((header, i) => (
-          <TableCell
-            key={`${data.caseId}-${i}`}
-            rowData={data}
-            header={header}
-            callback={callback}
-          />
-        ))}
+        {headers.map((header, i) => {
+          return (
+            <TableCell
+              key={`${data.extraData.caseId}-${i}`}
+              rowData={data}
+              header={header}
+              callback={callback}
+            />
+          );
+        })}
       </tr>
       {data.showDetails && (
         <CaseDetails
@@ -27,12 +34,6 @@ const ReceiptDisplayRow = props => {
       )}
     </React.Fragment>
   );
-};
-
-ReceiptDisplayRow.propTypes = {
-  headers: PropTypes.arrayOf(PropTypes.object).isRequired,
-  data: PropTypes.object.isRequired,
-  callback: PropTypes.objectOf(PropTypes.func).isRequired
 };
 
 export default ReceiptDisplayRow;
