25a95bb1112ea6f4648cfffd0f612176257974d5
Tim Best <timothy.best@hq.dhs.gov>
2019-10-29 09:58:55 -0400

    move end snooze action into an inline action

---
+++ src/controller/EndSnoozeForm.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import SnoozeForm from "./SnoozeForm";
+import { formatNotes } from "../view/util/formatNotes";
+import RestAPIClient from "../api/RestAPIClient";
+import { trackEvent } from "../matomo-setup";
+import { ActionModal } from "../view/util/ActionModal";
+import UsaButton from "../view/util/UsaButton";
+
+interface Props {
+  rowData: Case;
+  deSnooze: (receiptNumber: string) => void;
+}
+
+const EndSnoozeForm = (props: Props) => {
+  const [showDialog, setDialog] = useState(false);
+
+  const desnooze = (e: React.ChangeEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!props.rowData) {
+      console.error("resnooze called with out a vaild snooze option selected");
+      return;
+    }
+    props.deSnooze(props.rowData.receiptNumber);
+    closeModal();
+  };
+
+  const openModal = () => setDialog(true);
+  const closeModal = () => setDialog(false);
+
+  return (
+    <React.Fragment>
+      <ActionModal
+        isOpen={showDialog}
+        title={props.rowData.receiptNumber}
+        closeModal={closeModal}
+      >
+        <UsaButton onClick={desnooze} buttonStyle="secondary">
+          End Current Snooze
+        </UsaButton>
+      </ActionModal>
+      <UsaButton onClick={openModal} buttonStyle="secondary">
+        End
+      </UsaButton>
+    </React.Fragment>
+  );
+};
+
+export { EndSnoozeForm };
