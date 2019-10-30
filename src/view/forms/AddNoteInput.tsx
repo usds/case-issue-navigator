d5e960390501409b9f14a15a197426575f87fb1e
Tim Best <timothy.best@hq.dhs.gov>
2019-10-30 16:48:57 -0400

    add note to snooze case details

--- src/view/forms/AddNoteInput.tsx
+++ src/view/forms/AddNoteInput.tsx
@@ -5,10 +5,11 @@ import UsaButton from "../util/UsaButton";
 interface Props {
   onChange: (value: string) => void;
   value: string;
+  defaultShow?: boolean;
 }
 
 export default function AddNoteInput(props: Props) {
-  const [showNotes, setShowNotes] = useState(false);
+  const [showNotes, setShowNotes] = useState(props.defaultShow || false);
 
   if (showNotes) {
     return (
