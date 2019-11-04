64d9445d94973a277cfdb2cb22ef237290ddc091
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2019-11-04 11:49:20 -0500

    Make App.test.tsx tests pass by adding a Provider

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,30 +1,22 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import App, { UnconnectedApp } from "../App";
 import { MemoryRouter } from "react-router";
 import { Helmet } from "react-helmet";
 import { VIEWS } from "../controller/config";
-import { createStore } from "redux";
-import { rootReducer } from "../redux/create";
+import { store } from "../redux/create";
 import { Provider } from "react-redux";
 
 describe("App", () => {
-  it("should initially render with the correct title", () => {
-    const wrapper = shallow(
-      <UnconnectedApp
-        pageTitle="Case Issue Navigator"
-        setPageTitle={jest.fn()}
-      />
-    );
-    expect(wrapper.find("title").text()).toEqual("Case Issue Navigator");
-  });
   it("should show the correct title for Cases to Work", () => {
     mount(
       <MemoryRouter initialEntries={["/"]}>
-        <UnconnectedApp
-          pageTitle="Cases to Work | Case Issue Navigator"
-          setPageTitle={jest.fn()}
-        />
+        <Provider store={store}>
+          <UnconnectedApp
+            pageTitle="Cases to Work | Case Issue Navigator"
+            setPageTitle={jest.fn()}
+          />
+        </Provider>
       </MemoryRouter>
     );
     const helmet = Helmet.peek();
@@ -34,10 +26,12 @@ describe("App", () => {
     const setPageTitle = jest.fn();
     mount(
       <MemoryRouter initialEntries={["/snoozed-cases"]}>
-        <UnconnectedApp
-          pageTitle="Case Issue Navigator"
-          setPageTitle={setPageTitle}
-        />
+        <Provider store={store}>
+          <UnconnectedApp
+            pageTitle="Case Issue Navigator"
+            setPageTitle={setPageTitle}
+          />
+        </Provider>
       </MemoryRouter>
     );
     expect(setPageTitle).toHaveBeenCalledWith(
@@ -45,7 +39,6 @@ describe("App", () => {
     );
   });
   it("should connect to the store", () => {
-    const store = createStore(rootReducer);
     mount(
       <MemoryRouter initialEntries={["/snoozed-cases"]}>
         <Provider store={store}>
