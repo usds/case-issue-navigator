540e2e5f218e05fefd35de1a3eafe7bc064d09ed
Tim Best <timothy.best@hq.dhs.gov>
2019-11-22 10:43:39 -0500

    add information about case upload params

--- src/view/layout/Header.tsx
+++ src/view/layout/Header.tsx
@@ -70,6 +70,10 @@ const UnconnnectedHeader: React.FunctionComponent<HeaderProps> = props => {
       <ToastContainer />
       <PrimaryNavMenu views={VIEWS} />
       <FormattedDate label="Last Refresh" date={lastUpdated} />
+      <p>
+        Showing open cases older then 365 days. Excluding cases that have been
+        referred to BCU or FDNS.
+      </p>
     </React.Fragment>
   );
 };
