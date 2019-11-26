ef14697d2ecf3574110879785c00fba0b85ba435
Tim Best <timothy.best@hq.dhs.gov>
2019-11-26 15:50:59 -0500

    show and hide popper

--- src/view/util/DateRangePicker.tsx
+++ src/view/util/DateRangePicker.tsx
@@ -1,30 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Manager, Reference, Popper } from "react-popper";
 import "./DateRangePicker.scss";
 
 interface Props {}
 
 const DateRangePicker: React.FunctionComponent<Props> = props => {
+  const [show, setShow] = useState(false);
+
+  const togglePopper = () => setShow(!show);
+  const hidePopper = () => setShow(false);
+
   return (
     <Manager>
       <Reference>
         {({ ref }) => (
-          <button type="button" ref={ref}>
+          <button type="button" ref={ref} onClick={togglePopper}>
             Reference element
           </button>
         )}
       </Reference>
       <Popper placement="bottom">
-        {({ ref, style, placement, arrowProps }) => (
-          <div
+        {({ ref, style, placement }) => {
+          const popperStyle = {...style};
+          popperStyle.display = show ? "inherit" : "none";
+          return (      <div
             ref={ref}
-            style={style}
+            style={popperStyle}
             data-placement={placement}
             className="popper"
           >
             Popper element
+            <button type="button" ref={ref} onClick={hidePopper}>
+              hide
+            </button>
           </div>
-        )}
+        )
+        }
+    }
       </Popper>
     </Manager>
   );
