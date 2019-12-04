f6f7e1d079fe723eda8347057f15a85ae3be987c
Tim Best <timothy.best@hq.dhs.gov>
2019-12-04 16:42:25 -0500

    add case age filter to story book

---
+++ src/stories/forms/CaseAgeFilter.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { CaseAgeFilter } from "../../view/forms/CaseAgeFilter";
+
+storiesOf("CaseAgeFilter", module)
+  .add("Empty case list", () => (
+    <CaseAgeFilter
+      caselist={[{
+        caseCreation: "1/1/2014"
+      }]}
+      onStartChange={() => undefined}
+      onEndChange={() => undefined}
+      onSubmit={() => undefined}
+    />
+  ))
+
