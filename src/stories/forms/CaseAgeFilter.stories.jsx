bc5ad7c99518a6b5e0779a2c9baeb80ba62e0bf3
Tim Best <timothy.best@hq.dhs.gov>
2019-12-04 16:53:24 -0500

    cleanup

--- src/stories/forms/CaseAgeFilter.stories.jsx
+++ src/stories/forms/CaseAgeFilter.stories.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { CaseAgeFilter } from "../../view/forms/CaseAgeFilter";
 
-storiesOf("CaseAgeFilter", module)
-  .add("Empty case list", () => (
-    <CaseAgeFilter
-      caselist={[{
+storiesOf("CaseAgeFilter", module).add("Empty case list", () => (
+  <CaseAgeFilter
+    caselist={[
+      {
         caseCreation: "1/1/2014"
-      }]}
-      onStartChange={() => undefined}
-      onEndChange={() => undefined}
-      onSubmit={() => undefined}
-    />
-  ))
-
+      }
+    ]}
+    onStartChange={() => undefined}
+    onEndChange={() => undefined}
+    onSubmit={() => undefined}
+  />
+));
