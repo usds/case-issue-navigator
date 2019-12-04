8809983d074c33cffb5488830a0005086ea6a2e7
Tim Best <timothy.best@hq.dhs.gov>
2019-12-04 16:53:24 -0500

    cleanup

--- src/view/__test__/CaseList.test.tsx
+++ src/view/__test__/CaseList.test.tsx
@@ -8,61 +8,65 @@ import { CaseAgeFilter } from "../forms/CaseAgeFilter";
 import RestAPIClient from "../../api/RestAPIClient";
 
 const initialCases: Case[] = [
-    {
-      receiptNumber: "ABC123",
-      caseCreation: "Dec 12, 1984",
-      extraData: {
-        caseStatus: "",
-        caseState: "",
-        caseSubstatus: "",
-        channelType: "",
-        i90SP: true,
-        applicationReason: "some reason",
-        caseAge: "94 days",
-        caseId: "ABC123"
-      },
-      previouslySnoozed: false,
-      showDetails: false
-    }
-  ];
+  {
+    receiptNumber: "ABC123",
+    caseCreation: "Dec 12, 1984",
+    extraData: {
+      caseStatus: "",
+      caseState: "",
+      caseSubstatus: "",
+      channelType: "",
+      i90SP: true,
+      applicationReason: "some reason",
+      caseAge: "94 days",
+      caseId: "ABC123"
+    },
+    previouslySnoozed: false,
+    showDetails: false
+  }
+];
 
 describe("CaseList", () => {
   it("gets the cases of the correct type on initalization", () => {
     jest.spyOn(RestAPIClient.cases, "getCases");
     mount(
       <Provider store={store}>
-        <CaseList
-          snoozeState={"active"}
-        />
+        <CaseList snoozeState={"active"} />
       </Provider>
     );
     expect(store.getState().cases.type).toBe("active");
     expect(RestAPIClient.cases.getCases).toHaveBeenCalledWith(
-        "ACTIVE",
-        undefined,
-        undefined,
-        undefined
+      "ACTIVE",
+      undefined,
+      undefined,
+      undefined
     );
   });
   it("existing cases are cleared on filter submit", () => {
     jest.spyOn(RestAPIClient.cases, "getCases");
     const wrapper = mount(
       <Provider store={store}>
-        <CaseList
-          snoozeState={"active"}
-        />
+        <CaseList snoozeState={"active"} />
       </Provider>
     );
-    expect(wrapper.find('p').text()).toBe("Loading...");
+    expect(wrapper.find("p").text()).toBe("Loading...");
 
     store.dispatch(casesActionCreators.addCases(initialCases));
     store.dispatch(casesActionCreators.setIsLoading(false));
-    store.dispatch(casesActionCreators.setCaseCreationStart(new Date("1/1/2018")));
-    store.dispatch(casesActionCreators.setCaseCreationEnd(new Date("1/1/2019")));
+    store.dispatch(
+      casesActionCreators.setCaseCreationStart(new Date("1/1/2018"))
+    );
+    store.dispatch(
+      casesActionCreators.setCaseCreationEnd(new Date("1/1/2019"))
+    );
     wrapper.update();
 
     expect(wrapper.find(CaseAgeFilter).length).toBe(1);
-    wrapper.find(CaseAgeFilter).first().props().onSubmit();
+    wrapper
+      .find(CaseAgeFilter)
+      .first()
+      .props()
+      .onSubmit();
 
     expect(RestAPIClient.cases.getCases).toHaveBeenCalledWith(
       "ACTIVE",
@@ -70,19 +74,6 @@ describe("CaseList", () => {
       new Date("1/1/2018"),
       new Date("1/1/2019")
     );
-    expect(store.getState().cases.caselist.length).toBe(0)
+    expect(store.getState().cases.caselist.length).toBe(0);
   });
 });
-
-
-//   beforeEach(() => {
-//     store.dispatch(appStatusActionCreators.clearUser());
-//     store.dispatch(appStatusActionCreators.setIsInitializing(true));
-//   });
-    // store.dispatch(appStatusActionCreators.setIsInitializing(false));
-    // store.dispatch(
-    //   appStatusActionCreators.setUser({
-    //     name: "Bob",
-    //     id: "02010593-232d-443c-ba62-ee7a67302398"
-    //   })
-    // );
