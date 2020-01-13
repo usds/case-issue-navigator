8b8007058d2b2f0ddbca7a35e01dc71ce34b1057
Tim Best <timothy.best@hq.dhs.gov>
2020-01-13 14:54:26 -0500

    create sort function for note timestamps

--- src/utils/NoteUtils.ts
+++ src/utils/NoteUtils.ts
@@ -1,15 +1,14 @@
 export default class NoteUtils {
+  private static timestampDESC(a: Note, b: Note) {
+    return new Date(b.timestamp).valueOf() - new Date(a.timestamp).valueOf();
+  }
   static getFollowUp(notes: Note[] | undefined, subtype: SubType): Note | null {
     if (!notes) {
       return null;
     }
     const notesOfSubtype = notes
       .filter(note => note.subType === subtype)
-      .sort((a, b) => {
-        return (
-          new Date(b.timestamp).valueOf() - new Date(a.timestamp).valueOf()
-        );
-      });
+      .sort(NoteUtils.timestampDESC);
 
     const followUp = notesOfSubtype[0];
     return followUp ? followUp : null;
